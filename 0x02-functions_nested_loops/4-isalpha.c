9f1fa549fd205abc851a8d51e09b22407e6a2928
Ardiy <you@example.com>
2023-02-21 15:56:19 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,34 +3,13 @@
  * _isalpha - Enter point
  * Description: prints the uppercase alphabet
  */
-int _isalpha(int c) {
+int _isalpha(int c)
+{
 if (c >= 'a' && c <= 'z');
-return 1;  // c is a lowercase letter
-} else if (c >= 'A' && c <= 'Z') {
-return 1;  // c is an uppercase letter
+return (1);
+} else if (c >= 'A' && c <= 'Z')
+{
+return (1);
 } else {
-return 0;  // c is not a letter
-}
-
-
-int main() {
-    char test_char1 = 'g';
-    char test_char2 = 'T';
-    char test_char3 = '9';
-    if (_islower(test_char1)) {
-        printf("%c is a lowercase letter\n", test_char1);
-    } else {
-        printf("%c is not a lowercase letter\n", test_char1);
-    }
-    if (_islower(test_char2)) {
-        printf("%c is a lowercase letter\n", test_char2);
-    } else {
-        printf("%c is not a lowercase letter\n", test_char2);
-    }
-    if (_islower(test_char3)) {
-        printf("%c is a lowercase letter\n", test_char3);
-    } else {
-        printf("%c is not a lowercase letter\n", test_char3);
-    }
-    return 0;
+return (0);
 }
