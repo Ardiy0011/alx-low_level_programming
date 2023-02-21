a77401848c4b9d32296d459150091c0498772d19
Ardiy <you@example.com>
2023-02-21 14:33:43 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,18 @@
  * Description: prints 'main\n'
  * Return: Always 0 (success)
  */
-
+ 
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i = 0;
+char str[] = "_putchar\n";
+int i = 0;
 
-    while (str[i] != '\0')
-        {
-        char c = str[i];
+while (str[i] != '\0')
+{
+char c = str[i];
 
-        _putchar(c);
-        i++;
-        }
+_putchar(c);
+i++;
+}
 return (0);
 }
