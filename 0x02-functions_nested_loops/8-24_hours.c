138b80ddccf9aec8c05bf4c0e55522b043b43743
Ardiy <you@example.com>
2023-02-21 16:46:40 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,20 +4,42 @@
  * Description: prints every minute of the day
  * Return: void
  */
-void jack_bauer(void);
+void jack_bauer(void)
+{
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-int main()
-{
-    
-for (int hour = 0; hour < 24; hour++)
-{
-for (int minute = 0; minute < 60; minute++) {
-putchar((hour / 10) + '0');   // print tens digit of hour
-putchar((hour % 10) + '0');   // print ones digit of hour
-putchar(':');
-putchar((minute / 10) + '0'); // print tens digit of minute
-putchar((minute % 10) + '0'); // print ones digit of minute
-putchar('\n');               // print newline character
-}
-}
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
+	{
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
 }
