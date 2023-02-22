9fdff08b11c73a94e6639abf9e6f3cfc87513fad
Ardiy <you@example.com>
2023-02-21 16:44:49 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,38 +5,40 @@
  * Description: prints the 9 times table
  * Return: void
  */
-
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
+ */
 void times_table(void)
 {
-  int row, column, product, tens, ones;
+int row, column;
 
-  for (row = 0; row <= 9; row++)
-  {
-    for (column = 0; column <= 9; column++)
-    {
-      product = row * column;
-      tens = product / 10;
-      ones = product % 10;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+int product = row * column;
 
-      if (column == 0)
-      {
-        _putchar('0');
-      }
-      else if (product < 10)
-      {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(ones + '0');
-      }
-      else
-      {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(tens + '0');
-        _putchar(ones + '0');
-      }
-    }
-    _putchar('\n');
-  }
+if (column > 0)
+{
+_putchar(',');
+_putchar(' ');
 }
+
+if (product < 10)
+{
+_putchar(' ');
+}
+else
+{
+_putchar(product / 10 + '0');
+}
+_putchar(product % 10 + '0');
+}
+
+_putchar('\n');
+}
+}
+
