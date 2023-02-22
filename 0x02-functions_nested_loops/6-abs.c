c600ef87dcea55229836977842a54c7c83989379
Ardiy <you@example.com>
2023-02-21 16:19:04 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,21 +4,20 @@
  * _abs - Entry point
  * @ n: n is an integer
  * Description: prints the absolute value of an integer
- * Return: int
+ * Return: int - or +
  */
-
 int _abs(int n)
 {
-  if (n > 0)
-  {
-    return (n);
-  }
-  else if (n < 0)
-  {
-    return (-n);
-  }
-  else
-  {
-    return (n);
-  }
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
 }
