6c79b0be341922360c686bb6c419d6004237f271
Ardiy <you@example.com>
2023-02-21 16:09:25 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,18 +2,20 @@
 /**
  * _isalpha - Enter point
  * Description: prints the uppercase alphabet
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false
  */
 int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
 return (1);
-} 
+}
 else if (c >= 'A' && c <= 'Z')
 {
 return (1);
-} 
-else 
+}
+else
 {
 return (0);
 }
