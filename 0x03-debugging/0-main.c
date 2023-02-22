b821db476ef2d4d19c05e8f7f314ea6d0ea44274
Ardiy <you@example.com>
2023-02-22 15:51:45 -0800

    task 1-102

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * Description: a function that tests if integer is positive or negative
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
