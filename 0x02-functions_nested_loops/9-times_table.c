0d03526254ec5e1db9200124dcf2b4a71370b5e1
Ardiy <you@example.com>
2023-02-21 16:52:36 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,33 +6,28 @@
  */
 void times_table(void)
 {
-    int row, column;
+int row, column;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            int product = row * column;
-            
-            if (column > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            
-            if (product < 10)
-            {
-                _putchar(' ');
-            }
-            else
-            {
-                _putchar(product / 10 + '0');
-            }
-            
-            _putchar(product % 10 + '0');
-        }
-
-        _putchar('\n');
-    }
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+int product = row * column;
+if (column > 0)
+{
+_putchar(',');
+_putchar(' ');
 }
-
+if (product < 10)
+{
+_putchar(' ');
+}
+else
+{
+_putchar(product / 10 + '0');
+}
+_putchar(product % 10 + '0');
+}
+_putchar('\n');
+}
+}
