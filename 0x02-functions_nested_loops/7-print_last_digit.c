09046022c8ab519fc15d60bfc03825fb94e992a0
Ardiy <you@example.com>
2023-02-21 16:28:07 -0800

    tasks for variables and loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,17 +5,8 @@
  * Description: prints the last digit of a number
  * Return: integer
  */
-int get_last_digit(int num) {
-    int last_digit = num % 10;  // get the remainder of num divided by 10
-    return last_digit;
-}
-
-int main() {
-    int test_num1 = 12345;
-    int test_num2 = -987654321;
-    int test_num3 = 0;
-    printf("The last digit of %d is %d\n", test_num1, get_last_digit(test_num1));
-    printf("The last digit of %d is %d\n", test_num2, get_last_digit(test_num2));
-    printf("The last digit of %d is %d\n", test_num3, get_last_digit(test_num3));
-    return 0;
+int get_last_digit(int num)
+{
+int last_digit = num % 10; 
+return last_digit;
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,18 +2,20 @@
 
 void jack_bauer(void);
 
-int main() {
-    jack_bauer();
-    return 0;
+int main()
+{
+jack_bauer();
+return 0;
 }
-
-void jack_bauer(void) {
-    for (int hour = 0; hour < 24; hour++) {
-        for (int minute = 0; minute < 60; minute++) {
-            putchar((hour / 10) + '0');   // print tens digit of hour
-            putchar((hour % 10) + '0');   // print ones digit of hour
-            putchar(':');
-            putchar((minute / 10) + '0'); // print tens digit of minute
+void jack_bauer(void)
+{
+for (int hour = 0; hour < 24; hour++)
+{
+for (int minute = 0; minute < 60; minute++) {
+putchar((hour / 10) + '0');   // print tens digit of hour
+putchar((hour % 10) + '0');   // print ones digit of hour
+putchar(':');
+putchar((minute / 10) + '0'); // print tens digit of minute
             putchar((minute % 10) + '0'); // print ones digit of minute
             putchar('\n');               // print newline character
         }
