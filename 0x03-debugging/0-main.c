c535330fe136909fa89a334d1589499a124787ad
Ardiy <you@example.com>
2023-02-22 16:04:20 -0800

    task 1-102

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * Description: a function that tests if integer is positive or negative
+ * positive_or_negative - h
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
 int i;
-
 i = 0;
 positive_or_negative(i);
-
 return (0);
 }
