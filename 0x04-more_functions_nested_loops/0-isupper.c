2dfcbe52043211eb45b2c5bafc8ad1fe957162c2
Ardiy <you@example.com>
2023-02-23 18:02:47 -0800

    tasks more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,9 @@
  * Return: 1 if true, 0 if false
  */
 
-int _isupper(int c)//prototype
+int _isupper(int c)
 {
-if (c >= 65 && c <= 90)//if c is above the ascii alotment for lowercase alphabets
+if (c >= 65 && c <= 90)
 return (1);
 else
 return (0);
