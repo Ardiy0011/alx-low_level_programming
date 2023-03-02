64aac59d8a3d1514dfb1ab22d51376d7f832d991
Ardiy <you@example.com>
2023-03-01 17:56:45 -0800

    tasks for more variables

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -38,3 +38,44 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
+/**
+
+my alternative
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+*/
