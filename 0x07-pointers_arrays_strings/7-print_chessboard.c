e51e4adff8aad1f00fb6b5e534755c48832ecc00
Ardiy <you@example.com>
2023-03-06 16:10:18 -0800

    tasks more pointers 1 to crackme

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - Prints a chessboard.
  * @a: The chessboard to be printed.
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int indx1, indx2;
+int r, d;
 
-	for (indx1 = 0; a[indx1][7]; indx1++)
-	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-			_putchar(a[indx1][indx2]);
+for (r = 0; a[r][7]; r++)
+{
+for (d = 0; d < 8; d++)
+_putchar(a[r][d]);
 
-		_putchar('\n');
-	}
+_putchar('\n');
+}
 }
