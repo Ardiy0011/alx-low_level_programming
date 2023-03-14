8e4085d8a62b62a1cb6e7d0a56edd942004b8643
Ardiy <you@example.com>
2023-03-13 22:34:35 -0700

    taskfor malloc

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,75 +1,54 @@
-#include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
+char *create_array(unsigned int, char);
 
 /**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- *  * print_tab - Prints an array of string
- *
- *   * @tab: The array to print
- *
- *    *
- *
- *     * Return: nothing
- *
- *      */
-
-void print_tab(char **tab)
-
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
 {
+	unsigned int i;
 
-	    int i;
-
-
-
-	        for (i = 0; tab[i] != NULL; ++i)
-
-			    {
-
-				            printf("%s\n", tab[i]);
-
-					        }
-
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
-
-
 /**
+ * main - check the code .
  *
- *  * main - check the code for ALX School students.
- *
- *   *
- *
- *    * Return: 1 if an error occurred, 0 otherwise
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	char *buffer;
+	unsigned int size;
 
-	    char **tab;
-
-
-
-	        tab = strtow("      ALX School         #cisfun      ");
-
-		    if (tab == NULL)
-
-			        {
-
-					        printf("Failed\n");
-
-						        return (1);
-
-							    }
-
-		        print_tab(tab);
-
-			    return (0);
-
+	size = 72;
+	buffer = create_array(size, 'S');
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, size);
+	free(buffer);
+	return (0);
 }
