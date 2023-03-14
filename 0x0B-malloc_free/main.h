524af61518a645272f8080db1ec902ef3e14efe7
Ardiy <you@example.com>
2023-03-13 22:42:23 -0700

    taskfor malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *create_array(unsigned int size, char c)
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-malloc_free directory.
+ */
+
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
