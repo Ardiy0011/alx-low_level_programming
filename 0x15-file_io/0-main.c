5543e60d9caa653746e2c098f0c0d51e50281475
Ardiy <you@example.com>
2023-05-09 00:04:03 -0700

    fixes

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-* main - an entry point
- *@ac: number of arguments passed to the program
- *@av: array of strings representing the arguments
-* Return: Always 0.
-*/
+ * main - an entry point
+ *
+ * Return: Always 0.
+ */
 int main(int ac, char **av)
 {
-int res;
+    int res;
 
-if (ac != 3)
-{
-dprintf(2, "Usage: %s filename text\n", av[0]);
-exit(1);
-}
-res = create_file(av[1], av[2]);
-printf("-> %i)\n", res);
-return (0);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
