899121f28a8fd7fcd75b23ca27f8a905220b8650
Ardiy <you@example.com>
2023-06-16 01:30:44 +0000

    bug fixes

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	unsigned int i, b;
-	size_t length, sum;
+	size_t len, sum;
 	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
@@ -20,20 +20,20 @@ int main(int argc, char *argv[])
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	length = strlength(argv[1]);
-	p[0] = l[(length ^ 59) & 63];
-	for (i = 0, sum = 0; i < length; i++)
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, sum = 0; i < len; i++)
 		sum += argv[1][i];
 	p[1] = l[(sum ^ 79) & 63];
-	for (i = 0, b = 1; i < length; i++)
+	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
 	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < length; i++)
+	for (b = argv[1][0], i = 0; i < len; i++)
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	srand(b ^ 14);
 	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < length; i++)
+	for (b = 0, i = 0; i < len; i++)
 		b += argv[1][i] * argv[1][i];
 	p[4] = l[(b ^ 239) & 63];
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
