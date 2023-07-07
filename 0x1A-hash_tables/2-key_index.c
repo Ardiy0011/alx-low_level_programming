aed493d9f00782469f6742cefbce8a5ca961d64d
Ardiy <you@example.com>
2023-07-07 22:35:02 +0000

    bug fixes

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,15 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Compute the corresponding slot index for a given key
- * @key: The key used to compute the slot index
- * @size: The size of the hash table
- *
- * Return: The computed slot index
+ * key_index - takes the returned hash value fom has_djb2
+ * and computes corresponding slot using division hashing
+ * @str: string used to generate hash value
+ * Return: hash value
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int returned_value = hash_djb2(key);
-	unsigned long int slot = returned_value % size;
-	return (slot);
+
+    unsigned long int returned_value = hash_djb2(key);
+    unsigned long int slot = returned_value % size;
+    return (slot);
 }
