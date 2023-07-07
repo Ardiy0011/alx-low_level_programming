ae4105f620db7b10c96349fa79ed683157b3ede7
Ardiy <you@example.com>
2023-07-07 22:26:57 +0000

    bug fixes

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * main - check the code for
  *
@@ -7,9 +6,9 @@
  */
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+	return (EXIT_SUCCESS);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,8 @@
 #include "hash_tables.h"
-
 /**
  * key_index - Compute the corresponding slot index for a given key
  * @key: The key used to compute the slot index
  * @size: The size of the hash table
- *
  * Return: The computed slot index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
