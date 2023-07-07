8ad10ac2cdbfb4c841287955b4561b81916a3ca7
Ardiy <you@example.com>
2023-07-07 22:39:07 +0000

    bug fixes

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,4 +13,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int returned_value = hash_djb2(key);
 	unsigned long int slot = returned_value % size;
 	return (slot);
+
 }
