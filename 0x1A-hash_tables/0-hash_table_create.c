2ebdfb4bdb5957676469e7b7a351855952f6d917
Ardiy <you@example.com>
2023-07-07 22:10:45 +0000

    bug fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
     if (table == NULL)
-        return (NULL);
+    return (NULL);
 
     table->size = size;
     table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     }
 
     for (i = 0; i < table->size; i++)
-        table->array[i] = NULL;
+    table->array[i] = NULL;
 
     return (table);
 }
---
+++ 0x1A-hash_tables/linked.h
@@ -0,0 +1,24 @@
+#ifndef LINKED_H
+#define LINKED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * struct hash_table_s - defines the creation of teh linked list for collision
+ *
+ * @item: pointer to the hashtable items
+ * @next: pointer to thenext node
+ **/
+
+typedef struct LinkedList
+{
+     hash_node_t *item;
+     struct LinkedList *next;
+     LinkedList **overflow_buckets;
+} LinkedList;
+
+
+#endif /*LINKED_H*/
