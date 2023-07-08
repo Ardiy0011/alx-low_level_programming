0af9db6dca825c277671e4662d2fba9a4efc8939
Ardiy <you@example.com>
2023-07-08 19:53:26 +0000

    segemented fault with collision fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,8 +7,10 @@
  */
 void update_node_value(hash_node_t *node, const char *value)
 {
-    free(node->value);
-    node->value = strdup(value);
+
+	free(node->value);
+	node->value = strdup(value);
+
 }
 
 /**
@@ -42,6 +44,7 @@ hash_node_t *create_new_node(const char *key, const char *value)
 	new_node->next = NULL;
 
 	return (new_node);
+
 	}
 
 
@@ -54,34 +57,36 @@ hash_node_t *create_new_node(const char *key, const char *value)
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *current;
-    hash_node_t *new_node;
+	unsigned long int index;
+	hash_node_t *current;
+	hash_node_t *new_node;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-    return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
+	while (current != NULL)
 	{
-            update_node_value(current, value);
-            return (1);
-        }
-        current = current->next;
-    }
+	if (strcmp(current->key, key) == 0)
+	{
+		update_node_value(current, value);
+		return (1);
+	}
+	current = current->next;
+	}
 
-    new_node = create_new_node(key, value);
-    if (new_node == NULL)
-        return (0);
+	new_node = create_new_node(key, value);
+	if (new_node == NULL)
+	return (0);
 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
-    return (1);
+	return (1);
+
 }
