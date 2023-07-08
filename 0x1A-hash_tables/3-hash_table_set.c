fe7139442c09c9921ff5db90a86d8b1588b4190e
Ardiy <you@example.com>
2023-07-08 01:06:40 +0000

    collison handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to the hash table
@@ -9,48 +10,51 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = hash_function(key) % ht->size;
+	size_t key_len = strlen(key), i;
+	hash_node_t *slot_pt;
+	unsigned long int index;
+	unsigned char *unsigned_key = malloc((key_len + 1) * sizeof(unsigned char));
 
-	hash_node_t *item = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (item == NULL)
+	if (unsigned_key == NULL)
+		return 0;
+	for (i = 0; i <= key_len; i++)
+		unsigned_key[i] = (unsigned char)key[i];
+	index = key_index(unsigned_key, ht->size);
+	slot_pt = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (slot_pt == NULL)
 		return (0);
-	item->key = (char *)malloc(strlen(key) + 1);
-	if (item->key == NULL)
+	slot_pt->key = (char *)malloc(strlen(key) + 1);
+	if (slot_pt->key == NULL)
 	{
-		free(item);
-		return (0);
-	}
-	item->value = (char *)malloc(strlen(value) + 1);
-	if (item->value == NULL)
-	{
-		free(item->key);
-		free(item);
+		free(slot_pt);
 		return (0);
 	}
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-	if (ht->array[index] != NULL)
+	slot_pt->value = (char *)malloc(strlen(value) + 1);
+	if (slot_pt->value == NULL)
 	{
-		create_LL_and_handle_collision(ht, index, item);
+		free(slot_pt->key);
+		free(slot_pt);
+		return (0);
+	}
+	strcpy(slot_pt->key, key);
+	strcpy(slot_pt->value, value);
+	if (ht->array[index] != NULL)/*if the index of hash table isnt empty or has something run function that creates linked list*/
+	{
+		create_LL_and_handle_collision(ht, index, slot_pt);
 	}
 	else
 	{
-		ht->array[index] = item;
+		ht->array[index] = slot_pt;/*else place the value in the index of the hash table*/
 	}
 	return (1);
 }
 
-
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * create_LL_and_handle_collision - if collision create a linked list to store excess
+ * Return: none
  */
-int main(void)
+void create_LL_and_handle_collision()
 {
-    hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    return (EXIT_SUCCESS);
 }
+
