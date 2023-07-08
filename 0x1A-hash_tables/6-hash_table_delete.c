f3c47f3b8c95baa0ec4dc94be8537c20480ddd30
Ardiy <you@example.com>
2023-07-08 19:09:18 +0000

    segemented fault with collision fixed

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,7 +9,7 @@
 void hash_table_delete(hash_table_t *ht)
 {
  hash_table_t *head = ht;
- hash_node_t *node, *tmp;
+ hash_node_t *node, *temp;
  unsigned long int i;
 
  for (i = 0; i < ht->size; i++)
@@ -19,11 +19,11 @@ void hash_table_delete(hash_table_t *ht)
    node = ht->array[i];
    while (node != NULL)
    {
-    tmp = node->next;
+    temp = node->next;
     free(node->key);
     free(node->value);
     free(node);
-    node = tmp;
+    node = temp;
    }
   }
  }
