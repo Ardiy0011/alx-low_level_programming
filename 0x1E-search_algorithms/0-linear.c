4e01c7f4f9c3c3352db1ef92388cf16e63e3fa57
Ardiy0011 <ardiyquarsh@gmail.com>
2023-10-09 22:51:03 +0000

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search.h"
+/**
+ * linear_search - runs linear search in an array of vals
+ * @array: pointer to the array
+ * @size: first value in the array
+ * @value: the value
+ * Return: -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+	{
+	return (-1);
+
+	}
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+	if (array[i] == value)
+	{
+		return (i);
+	}
+	}
+
+	return (-1);
+}
+
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+tasks for linear and binary search
